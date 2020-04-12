a2962a4cd2d40cac7cf7b58cd660989dbcefa4b4
ali  geran <aligeranmayeh9797@gmail.com>
2020-04-13 02:19:23 +0430

    Add react states for sign up and sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export default class SignIn extends Component {
+    constructor() {
+        super();
+
+        this.state ={         
+            userName: null , 
+            password : null 
+        }
+    }
     render() {
         return (
             <div className = "FormCenter">
@@ -8,11 +16,11 @@ export default class SignIn extends Component {
               <div className="FormField">
               <div className="FormField">
                 <label className="FormField__Label"  htmlFor="username">نام کاربری</label>
-                <input type="text" id="username" className="FormField__Input" placeholder="نام کاربری خود را وارد کنید" name="userName"/>
+                <input type="text" id="username" className="FormField__Input" placeholder="نام کاربری خود را وارد کنید" name="userName" value = {this.state.userName}/>
               </div>
               <div className="FormField">
                 <label className="FormField__Label"  htmlFor="password">گذرواژه</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="گذرواژه خود را وارد کنید" name="password"/>
+                <input type="password" id="password" className="FormField__Input" placeholder="گذرواژه خود را وارد کنید" name="password" value = {this.state.password}/>
               </div>
 
                 <div className="FormField">
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,41 +1,56 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+    constructor() {
+        super();
+
+        this.state ={
+            firstName: null ,
+            lastName: null , 
+            userName: null , 
+            password : null , 
+            email : null ,
+            phonenumber : null , 
+            postalcode : null ,
+            address: null
+        }
+    }
+
     render() {
         return (
             <div className = "FormCenter">
             <form  className="FormFields">
               <div className="FormField">
                 <label className="FormField__Label"  htmlFor="firstname">نام</label>
-                <input type="text" id="firstname" className="FormField__Input" placeholder="نام خود را وارد کنید" name="firstName"/>
+                <input type="text" id="firstname" className="FormField__Input" placeholder="نام خود را وارد کنید" name="firstName" value = {this.state.firstName}/>
               </div>
               <div className="FormField">
                 <label className="FormField__Label"  htmlFor="lastname">نام خانوادگی</label>
-                <input type="text" id="lastname" className="FormField__Input" placeholder="نام خانوادگی خود را وارد کنید" name="lastName"/>
+                <input type="text" id="lastname" className="FormField__Input" placeholder="نام خانوادگی خود را وارد کنید" name="lastName" value = {this.state.lastName}/>
               </div>
               <div className="FormField">
                 <label className="FormField__Label"  htmlFor="username">نام کاربری</label>
-                <input type="text" id="username" className="FormField__Input" placeholder="نام کاربری خود را وارد کنید" name="userName"/>
+                <input type="text" id="username" className="FormField__Input" placeholder="نام کاربری خود را وارد کنید" name="userName" value = {this.state.userName}/>
               </div>
               <div className="FormField">
                 <label className="FormField__Label"  htmlFor="password">گذرواژه</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="گذرواژه خود را وارد کنید" name="password"/>
+                <input type="password" id="password" className="FormField__Input" placeholder="گذرواژه خود را وارد کنید" name="password" value = {this.state.password}/>
               </div>
               <div className="FormField">
                   <label className="FormField__Label" htmlFor="email">پست الکترونیک</label>
-                  <input type="email" id="email" className ="FormField__Input" placeholder="پست الکترونیک خود را وارد کنید" name="email"/>
+                  <input type="email" id="email" className ="FormField__Input" placeholder="پست الکترونیک خود را وارد کنید" name="email"value = {this.state.email}/>
                 </div>
                 <div className="FormField">
                   <label className="FormField__Label" htmlFor="phonenumber">شماره تماس</label>
-                  <input type="number" id="phonenumber" className="FormField__Input" placeholder="شماره تماس خود را وارد کنید" name="phonenumber" />
+                  <input type="number" id="phonenumber" className="FormField__Input" placeholder="شماره تماس خود را وارد کنید" name="phonenumber" value = {this.state.phonenumber} />
                 </div>
                 <div className="FormField">
                   <label className="FormField__Label" htmlFor="postalcode">کد پستی</label>
-                  <input type="text" id="postalcode" className="FormField__Input" placeholder="کد پستی خود را وارد کنید" name="postalcode"/>
+                  <input type="text" id="postalcode" className="FormField__Input" placeholder="کد پستی خود را وارد کنید" name="postalcode" value = {this.state.postalcode}/>
                 </div>
                 <div className="FormField">
                   <label className="FormField__Label" htmlFor="address">آدرس</label>
-                  <textarea rows= "4" id="address"className="FormField__Input" placeholder="آدرس خود را وارد کنید" name="address"/>
+                  <textarea rows= "4" id="address"className="FormField__Input" placeholder="آدرس خود را وارد کنید" name="address" value = {this.state.address}/>
                 </div>
                 <div className="FormField">
                     <button className="FormField__Button mr-20">ثبت نام</button>
