bd49c7ace60eec966701aeea75e215d3f214c4dd
ali  geran <aligeranmayeh9797@gmail.com>
2020-04-15 10:15:28 +0430

    Add sim[le navbars

--- book_info/src/App.js
+++ book_info/src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className = "shlfNav"><h2>shlf</h2></div>
+      <div className = "logo">
+        <ul className = "navbar">
+          <li><a href = "#">خانه</a></li>
+          <li><a href = "#">خرید کتاب</a></li>
+          <li><a href = "#">قرض کتاب</a></li>
+          <li><a href = "#">دسته بندی کتاب ها</a></li>
+          <li><a href = "#">درباره ما</a></li>
+        </ul>
+      </div>
+    </>
   );
 }
 
