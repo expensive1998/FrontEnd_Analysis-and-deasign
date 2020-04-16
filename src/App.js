72f1ac7d2209d23434149963ffb9458ab4f2c828
Hossein Alipour <hoseinscopion62@gmail.com>
2020-04-17 03:20:28 +0430

    Finish Writein page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import MainPage from './MainPage';
 import {BrowserRouter as Router , Route , Switch , Link , Redirect } from 'react-router-dom';
 import Writeinbook from './WriteInBook/Writeinbook'
+import Writeinbooklend from './WriteInBook/Writeinbooklend'
 
 class App extends Component{
   render(){
@@ -9,6 +10,7 @@ class App extends Component{
       <Router>
         <Route exact path ="/" component={MainPage} />
         <Route exact path ="/writein" component = {Writeinbook} />
+        <Route exact path ="/Borrow" component = {Writeinbooklend} />
       </Router>
     );
   }
